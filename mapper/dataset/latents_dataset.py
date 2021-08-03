ac428493c9c3dd980debf33b613ab5b9fc312b9b
Yubin-Cho <74402562+yubin1219@users.noreply.github.com>
2021-08-03 14:13:07 +0900

    Update latents_dataset.py

--- mapper/dataset/latents_dataset.py
+++ mapper/dataset/latents_dataset.py
@@ -4,19 +4,17 @@ class LatentsDataset(Dataset):
 	def __init__(self, latents, opts):
 		self.latents = latents
 		self.opts = opts
-
 	def __len__(self):
 		return self.latents.shape[0]
-
+	
 	def __getitem__(self, index):
 		w = self.latents[index]
-    		if w[0][0]==1:
-      			t = "blonde hair"
-    		elif w[0][1]==1:
-      			t= "pink hair"
-    		elif w[0][2]==1:
-      			t = "blue hair"
-    		elif w[0][3]==1:
-     		 	t = "black hair"
-    		return [w,t]
-
+		if w[0][0]==1:
+			t = "blonde hair"
+		elif w[0][1]==1:
+			t= "pink hair"
+		elif w[0][2]==1:
+			t = "blue hair"
+		elif w[0][3]==1:
+			t = "black hair"
+		return [w,t]
