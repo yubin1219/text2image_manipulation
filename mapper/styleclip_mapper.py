ce32ed0e3055441002155f605bbd3d3f3421326d
Yubin-Cho <74402562+yubin1219@users.noreply.github.com>
2021-08-19 13:03:46 +0900

    Update styleclip_mapper.py

--- mapper/styleclip_mapper.py
+++ mapper/styleclip_mapper.py
@@ -42,22 +42,11 @@ class StyleCLIPMapper(nn.Module):
 			ckpt = torch.load(self.opts.stylegan_weights)
 			self.decoder.load_state_dict(ckpt['g_ema'], strict=False)
 
-	def forward(self, x, resize=True, latent_mask=None, input_code=False, randomize_noise=True,
-	            inject_latent=None, return_latents=False, alpha=None):
+	def forward(self, x, resize=True, input_code=False, randomize_noise=True, return_latents=False):
 		if input_code:
 			codes = x
 		else:
 			codes = self.mapper(x)
-
-		if latent_mask is not None:
-			for i in latent_mask:
-				if inject_latent is not None:
-					if alpha is not None:
-						codes[:, i] = alpha * inject_latent[:, i] + (1 - alpha) * codes[:, i]
-					else:
-						codes[:, i] = inject_latent[:, i]
-				else:
-					codes[:, i] = 0
 
 		input_is_latent = not input_code
 		images, result_latent = self.decoder([codes],
