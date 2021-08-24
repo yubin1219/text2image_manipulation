e1fbe5039fe34325efe833181a0ca8879adde70c
Yubin-Cho <74402562+yubin1219@users.noreply.github.com>
2021-08-24 18:17:32 +0900

    Update inference.py

--- mapper/inference.py
+++ mapper/inference.py
@@ -198,9 +198,9 @@ def module_combine(test_opts):
   print("time : %.4f" %(end - start))
 
   result = torch.cat(images)
-  image = ToPILImage()(make_grid(result.detach().cpu(), normalize=True, scale_each=True, value_range=(-1, 1), padding=0))
-  h, w = image.size
-  return result, image.resize((h // 2, w // 2))
+  #image = ToPILImage()(make_grid(result.detach().cpu(), normalize=True, scale_each=True, value_range=(-1, 1), padding=0))
+  #h, w = image.size
+  return result
 
 if __name__ == '__main__':
   """
@@ -222,5 +222,8 @@ if __name__ == '__main__':
                   "texts": ["Emma Stone","wavy", "blonde"]}
   #test_opts = Namespace(**test_options)
   test_opts = TestOptions().parse()
-  result, images = module_combine(test_opts)
+  result = module_combine(test_opts)
+  image = ToPILImage()(make_grid(result.detach().cpu(), normalize=True, scale_each=True, value_range=(-1, 1), padding=0))
+  h, w = image.size
+  image.resize((h // 2, w // 2))
   images.save("results.png")
