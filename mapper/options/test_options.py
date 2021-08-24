eb507a288f32164ad92c5ede23006d6d9add2dcb
Yubin-Cho <74402562+yubin1219@users.noreply.github.com>
2021-08-24 19:09:23 +0900

    Update test_options.py

--- mapper/options/test_options.py
+++ mapper/options/test_options.py
@@ -13,7 +13,7 @@ class TestOptions:
 		self.parser.add_argument('--latent_path', default='test_female.pt', type=str, help="The latents for the test")
 		self.parser.add_argument('--w_num', default=60, type=int, help="The latents number")
 		self.parser.add_argument('--modules', default="celeb_female,hair_sum,color_sum", type=str, help="Which modules will be combined?")
-		self.parser.add_argument('--texts', default="Elsa,wavy,pink", type=str help="The latents for the test")
+		self.parser.add_argument('--texts', default="Elsa,wavy,pink", type=str, help="The latents for the test")
 
 
 	def parse(self):
