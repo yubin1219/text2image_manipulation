107136becfb1ab9f29902d04aa4010370cc95935
Yubin-Cho <74402562+yubin1219@users.noreply.github.com>
2021-08-26 15:43:42 +0900

    Update test_options.py

--- mapper/options/test_options.py
+++ mapper/options/test_options.py
@@ -9,8 +9,8 @@ class TestOptions:
 		# arguments for inference script
 		self.parser.add_argument('--exp_dir', default='results/', type=str, help='Path to experiment output directory')
 		self.parser.add_argument('--weights_download', default=True, action='store_false', help='Download weights used for testing')
-		self.parser.add_argument('--new_latents', default=False, action='store_true', help='Download weights used for testing')
-		self.parser.add_argument('--new_image_path', default=None, type=str, help='Download weights used for testing')
+		self.parser.add_argument('--new_latent', default=False, action='store_true', help='Use new latent')
+		self.parser.add_argument('--new_image_path', default=None, type=str, help='New image path')
 		self.parser.add_argument('--intermediate_outputs', default=True, action='store_false', help='Whether to also visualize input and outputs side-by-side')
 		self.parser.add_argument('--latent_path', default='test_female.pt', type=str, help="The latents for the test")
 		self.parser.add_argument('--w_num', default=60, type=int, help="The latents number")
