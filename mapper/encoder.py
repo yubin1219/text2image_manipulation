5cf61c0ac4ed73e4760a1782f182e9eb32962136
Yubin-Cho <74402562+yubin1219@users.noreply.github.com>
2021-08-26 14:44:44 +0900

    Update encoder.py

--- mapper/encoder.py
+++ mapper/encoder.py
@@ -1,6 +1,36 @@
+from argparse import Namespace
+import sys
+import numpy as np
+from PIL import Image
+import torchvision.transforms as transforms
+import torch
+
+sys.path.append(".")
+sys.path.append("..")
+
+from utils.common import tensor2im
 from mapper.psp import pSp
 import dlib
-from aligment import align_face
+from mapper.aligment import align_face
+
+def run_alignment(image_path):
+  predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+  aligned_image = align_face(filepath=image_path, 
+                             predictor=predictor) 
+  print("Aligned image has shape: {}".format(aligned_image.size))
+  return aligned_image 
+
+def display_alongside_source_image(result_image, source_image):
+  res = np.concatenate([np.array(source_image.resize(resize_dims)),
+                        np.array(result_image.resize(resize_dims))], axis=1)
+  return Image.fromarray(res)
+
+def run_on_batch(inputs, net):
+  images, latents = net(inputs.to("cuda").float(), randomize_noise=False, return_latents=True)
+  if experiment_type == 'cars_encode':
+    images = images[:, :, 32:224, :]
+  return images, latents
+
 
 def encoder(options):
   model_path = "e4e_ffhq_encode.pt"
@@ -11,3 +41,22 @@ def encoder(options):
   opts = Namespace(**opts)
   net = pSp(opts)
   net.eval().to(device)
+  
+  image_path = options.new_image_path
+  original_image = Image.open(image_path)
+  original_image = original_image.convert("RGB")
+  input_image = run_alignment(image_path)
+  input_image.resize((256,256))
+  
+  img_transforms = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+  
+  transformed_image = img_transforms(input_image)
+  with torch.no_grad():
+    images, latents = run_on_batch(transformed_image.unsqueeze(0), net)
+    result_image, latent = images[0], latents[0]
+  torch.save(latents, 'latents.pt')
+  # Display inversion:
+  display_alongside_source_image(tensor2im(result_image), input_image)
