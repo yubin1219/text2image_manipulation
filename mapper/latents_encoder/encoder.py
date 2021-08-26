0c9bd976e1e8d16989b041fe560f81c497a38e04
Yubin-Cho <74402562+yubin1219@users.noreply.github.com>
2021-08-26 15:22:50 +0900

    Update encoder.py

--- mapper/latents_encoder/encoder.py
+++ mapper/latents_encoder/encoder.py
@@ -36,7 +36,6 @@ def encoder(new_img_path):
   device = "cuda" if torch.cuda.is_available() else "cpu"
   
   model_path = "e4e_ffhq_encode.pt"
-  ensure_checkpoint_exists(model_path)
   ckpt = torch.load(model_path, map_location=device)
   opts = ckpt['opts']
   opts['checkpoint_path'] = model_path
